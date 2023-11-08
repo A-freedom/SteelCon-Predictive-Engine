a47f6ccd52af2df6b324d307ff1b50eb67331654
A-freedom <ali.musa.omran+github@gmail.com>
2023-11-08 21:20:28 +0300

    ازالة كود غير ضروري

--- main.py
+++ main.py
@@ -9,53 +9,7 @@ os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"
 
 # %%
 # Load the dataset
-df = pd.read_csv('DATA/data_R_FCST.csv', header=0)
-
-
-# %%
-X = df[['b (mm)','h (mm)','t (mm)','L (mm)','fy (MPa)','Ea (Gpa)','fc (MPa)']]
-y = df['N Test (kN)']
-
-# %%
-# Scale features and target variable to [0, 1]
-scaler = StandardScaler()
-X = scaler.fit_transform(X)
-# y = scaler.fit_transform([y])
-# %%
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1,random_state=20)
-
-# %%
-model = tf.keras.Sequential([
-    tf.keras.layers.Dense(75, activation='tanh'),
-    tf.keras.layers.Dense(75),
-    tf.keras.layers.Dense(1)  # Output layer for regression
-])
-
-# %%
-# Compile the model
-custom_optimizer = tf.keras.optimizers.Adam(learning_rate=0.001)
-model.compile(optimizer=custom_optimizer, loss='mean_absolute_percentage_error',  metrics=["mean_absolute_percentage_error"])
-
-# %%
-# Early stopping callback
-early_stopping = EarlyStopping(monitor='val_loss', patience=5000, restore_best_weights=True)
-# Model checkpoint callback
-checkpoint = ModelCheckpoint("best_model.h5", save_best_only=True)
-# TensorBoard callback for profiling
-tensorboard = TensorBoard(log_dir="logs/")
-# %%
-import pandas as pd
-import tensorflow as tf
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
-from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint, TensorBoard
-import os
-os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"
-
-# %%
-# Load the dataset
-df = pd.read_csv('DATA/data_R_FCST.csv', header=0)
-
+df = pd.read_csv('Graduation_Project/DATA/data_R_FCST.csv', header=0)
 
 # %%
 X = df[['b (mm)','h (mm)','t (mm)','L (mm)','fy (MPa)','fc (MPa)']]
@@ -107,25 +61,3 @@ loss, mae = model.evaluate(X_test, y_test)
 # %%
 # Save the model
 model.save('my_model')
-
-# %%
-# Make predictions
-y_pre = pd.DataFrame(model.predict(X_test))
-# Calculate and save errors
-error = (y_test - y_pre) / y_test * 100
-error = error.sort_values(by=0)
-error.to_csv('error.csv',index=False)
-loss, mae = model.evaluate(X_test, y_test)
-# print("Test Loss:", loss, "Test MAE:", mae)
-
-# %%
-# Save the model
-model.save('my_model')
-
-# %%
-# Make predictions
-y_pre = pd.DataFrame(model.predict(X_test))
-# Calculate and save errors
-error = (y_test - y_pre) / y_test * 100
-error = error.sort_values(by=0)
-error.to_csv('error.csv',index=False)
