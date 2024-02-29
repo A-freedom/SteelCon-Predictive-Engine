afd1c8e7c207f6f8a6f06c92b8a6c718ff1f34cc
A-freedom <ali.musa.omran+github@gmail.com>
2024-02-29 17:34:06 +0300

    moving all the model work into sprite folder

--- web_server/app.py
+++ web_server/app.py
@@ -1,12 +1,11 @@
 import pickle
 
 import pandas as pd
+import tensorflow as tf
 from flask import Flask, request, jsonify, render_template
 from flask_cors import CORS  # Import CORS from flask_cors module
-from sklearn.externals import joblib
 
-loaded_model = joblib.load('../model_work/my_model/best_model.pkl')
-
+loaded_model = tf.keras.models.load_model('../model_work/my_model/best_model.h5')
 with open('../model_work/my_model/data_scaler.pkl', 'rb') as f:
     scaler = pickle.load(f)
 
